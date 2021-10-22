c9866c7f65f8fbf8a2b55ce25cffc980113da05f
sabrina.fiore <sabrina.fiore@finconsgroup.com>
2021-10-22 11:31:24 +0200

    add dynamic form

--- src/app/components/driven/driven.component.ts
+++ src/app/components/driven/driven.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './driven';
 
 @Component({
   selector: 'app-driven',
   templateUrl: './driven.component.html',
   styleUrls: ['./driven.component.scss']
 })
+
 export class DrivenComponent implements OnInit {
   
   name: string;
   age: number;
+  city: string;
+  user: User;
+
   users = [
     {
       name: "Lucio",
       age: 44,
+      city: "Padova"
     }
   ];
 
@@ -27,8 +34,12 @@ export class DrivenComponent implements OnInit {
     input.focus();
   }
 
-  addUser(form) {
+  addUser(form: NgForm) {
     this.users.push(form.value);
     console.log("test on: " + form.value.name);
+  }
+
+  setActive(user: User) {
+    this.user = user;
   }
 }
